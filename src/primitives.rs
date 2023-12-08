3516115b222c84b2fae0e1321a532900770b7360
TheBigCheese <32036861+13ros27@users.noreply.github.com>
2023-12-08 18:39:52 +0000

    Try getting Ray3d directly

--- src/primitives.rs
+++ src/primitives.rs
@@ -246,8 +246,8 @@ pub mod rays {
         }
     }
 
-    impl From<bevy_math::prelude::Ray3d> for Ray3d {
-        fn from(ray: bevy_math::prelude::Ray3d) -> Self {
+    impl From<bevy_math::ray::Ray3d> for Ray3d {
+        fn from(ray: bevy_math::ray::Ray3d) -> Self {
             Ray3d::new(ray.origin, ray.direction)
         }
     }
